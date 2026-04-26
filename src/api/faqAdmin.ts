44ac9a06c38d09330c2794d61d07be447a44d590
maszhar <fikrimustofa024@gmail.com>
2026-04-26 08:18:37 +0700

    perbaikan: batasi akses get faq admin untuk admin

--- src/api/faqAdmin.ts
+++ src/api/faqAdmin.ts
@@ -1,8 +1,10 @@
 import * as express from "express";
 
+import { auth } from "~/middlewares";
 import { KontrolFaq } from "~/modules/faq/business/KontrolFaq";
+import { PeranPengguna } from "~/modules/otentikasi/domain/PeranPengguna";
 
 const kontrolFaq = KontrolFaq.instance;
 
 export const routerFaqAdmin = express.Router();
-routerFaqAdmin.get("/", (req, res) => kontrolFaq.getFaqs(req, res));
+routerFaqAdmin.get("/", auth([PeranPengguna.Admin]), (req, res) => kontrolFaq.getFaqs(req, res));
